2d184fc1d4c6f5c4cc96afefd102e40181a2d6f5
0nn7a <mohonaney@gmail.com>
2025-07-21 16:10:28 +0800

    建立 github page ci 自動部署

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/portfolio-website/" : "./",
+  base: "/portfolio-website/",
   plugins: [
     vue(),
     AutoImport({
